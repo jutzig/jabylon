0d7f0e60632739569db5ea70abcebbff7d9d2513
Sascha Vogt <sascha@vogt-neuenbuerg.de>
2012-05-03 00:15:52 +0200

    added FIXME for discussion

--- de.jutzig.jabylon.ui/src/main/java/de/jutzig/jabylon/ui/config/internal/general/UserConfig.java
+++ de.jutzig.jabylon.ui/src/main/java/de/jutzig/jabylon/ui/config/internal/general/UserConfig.java
@@ -203,7 +203,7 @@ public class UserConfig extends AbstractConfigSection<Workspace> implements Conf
 	}
 
 	private void initializeUserManagement() {
-		view = connector.openTransaction();
+		view = connector.openTransaction(); //FIXME: why is this needed to be able to read the selected users permissions?
 
 		if(view.hasResource(ServerConstants.USERS_RESOURCE)) {
 			CDOResource resource = view.getResource(ServerConstants.USERS_RESOURCE);
