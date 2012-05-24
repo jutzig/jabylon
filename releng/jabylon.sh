7305d7bc3cfeb4fb0e5cf938dc84a3efcc89a874
Johannes Utzig <jutzig.dev@googlemail.com>
2012-05-25 01:13:56 +0200

    close #24 added console option and added check if instance is already running

--- releng/jabylon.sh
+++ releng/jabylon.sh
@@ -1,26 +1,49 @@
 #!/bin/sh
 
 
-pidfile=/tmp/jabylon.pid
+pidfile=jabylon.pid
 
 VMARGS="-XX:MaxPermSize=256m -Xms40m -Xmx768m -Dorg.eclipse.equinox.http.jetty.http.port=8080 -Declipse.ignoreApp -Dosgi.noShutdown"
 
 
-if [ $# -gt 0 ]; then
-	if [ $1 = start ]; then
-		echo "Starting Jabylon"
-		PROGRAM="java $VMARGS -jar plugins/org.eclipse.equinox.launcher_1.2.0.v20110502.jar"
-		nohup $PROGRAM > wrapper.log 2>&1 &
-		PID=$!
-		echo $PID > "$pidfile"
-		echo "Jabylon running as $PID"
+usage()
+{
+    echo "Usage: jabylon [start|stop|console]"
+}
+
+start()
+{
+	if [ -f $pidfile ]; then 
+		echo "Jabylon is already running"
+		exit 1
 	fi
-	if [ $1 = stop ]; then
-		pid2kill=`cat $pidfile`
-		kill -9 $pid2kill
-		echo "shut down Jabylon"
-		rm $pidfile
-	fi	  
+       	echo "Starting Jabylon"
+	PROGRAM="java $VMARGS -jar plugins/org.eclipse.equinox.launcher_1.2.0.v20110502.jar"
+	nohup $PROGRAM > wrapper.log 2>&1 &
+	PID=$!
+	echo $PID > "$pidfile"
+	echo "Jabylon running as $PID"
+}
+
+if [ $# -gt 0 ]; then	
+    case $1 in
+        start )			start
+                                ;;
+        stop )    		pid2kill=`cat $pidfile`
+				kill -9 $pid2kill
+				echo "shutting down Jabylon"
+				rm $pidfile
+                                ;;
+        console )           	$VMARG=$VMARGS -Dosgi.console= -Declipse.consoleLog=true
+			       	echo "Starting Jabylon"
+				PROGRAM="java $VMARGS -jar plugins/org.eclipse.equinox.launcher_1.2.0.v20110502.jar"
+				$PROGRAM
+                                ;;
+        * )                     usage
+                                exit 1
+    esac  
 else
-    echo "Usage: jabylon [start|stop]"
+    usage
+    exit 1	
 fi
+
