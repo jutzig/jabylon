eee613af96f0a9f74514b4557378b020350edb0c
Johannes Utzig <jutzig.dev@googlemail.com>
2012-06-05 00:32:14 +0200

    close #26 auto cleanup job implemented

---
+++ de.jutzig.jabylon.review.standard/src/main/java/de/jutzig/jabylon/review/standard/ReviewActivator.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package de.jutzig.jabylon.review.standard;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Plugin;
+import org.eclipse.core.runtime.Status;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+import de.jutzig.jabylon.resources.persistence.PropertyPersistenceService;
+
+/**
+ * @author Johannes Utzig (jutzig.dev@googlemail.com)
+ *
+ */
+public class ReviewActivator extends Plugin implements BundleActivator {
+	private static ReviewActivator INSTANCE;
+	public static final String PLUGIN_ID = "de.jutzig.jabylon.review.standard";
+	private PropertyPersistenceService persistenceService;
+	private ServiceReference<PropertyPersistenceService> persistenceReference;
+	
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		INSTANCE = this;
+		
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+		INSTANCE = null;
+		persistenceService = null;
+		if(persistenceReference!=null)
+			context.ungetService(persistenceReference);
+	}
+
+
+	public static ReviewActivator getDefault() {
+		return INSTANCE;
+	}
+	
+	public PropertyPersistenceService getPersistenceService()
+	{
+		if(persistenceService==null)
+		{
+			persistenceReference = getBundle().getBundleContext().getServiceReference(PropertyPersistenceService.class);
+			persistenceService = getBundle().getBundleContext().getService(persistenceReference);
+		}
+		return persistenceService;
+	}
+	
+	public void log(IStatus status)
+	{
+		getLog().log(status);
+	}
+	
+	public void log(String message, Throwable cause)
+	{
+		IStatus status = new Status(IStatus.ERROR, PLUGIN_ID, message, cause);
+		log(status);
+	}
+	
+	public void log(String message, int severity)
+	{
+		IStatus status = new Status(severity, PLUGIN_ID, message);
+		log(status);
+	}
+}
---
+++ de.jutzig.jabylon.review.standard/src/main/java/de/jutzig/jabylon/review/standard/cleanup/TranslationCleanupJob.java
@@ -0,0 +1,126 @@
+/**
+ * 
+ */
+package de.jutzig.jabylon.review.standard.cleanup;
+
+import java.text.MessageFormat;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.cdo.view.CDOView;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.resource.Resource;
+
+import de.jutzig.jabylon.cdo.server.ServerConstants;
+import de.jutzig.jabylon.properties.Project;
+import de.jutzig.jabylon.properties.ProjectLocale;
+import de.jutzig.jabylon.properties.ProjectVersion;
+import de.jutzig.jabylon.properties.Property;
+import de.jutzig.jabylon.properties.PropertyFile;
+import de.jutzig.jabylon.properties.PropertyFileDescriptor;
+import de.jutzig.jabylon.properties.Workspace;
+import de.jutzig.jabylon.resources.persistence.PropertyPersistenceService;
+import de.jutzig.jabylon.review.standard.ReviewActivator;
+import de.jutzig.jabylon.scheduler.JobContextUtil;
+import de.jutzig.jabylon.scheduler.JobExecution;
+
+/**
+ * @author Johannes Utzig (jutzig.dev@googlemail.com)
+ *
+ */
+public class TranslationCleanupJob implements JobExecution {
+
+	/**
+	 * 
+	 */
+	public TranslationCleanupJob() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see de.jutzig.jabylon.scheduler.JobExecution#run(java.util.Map)
+	 */
+	@Override
+	public void run(Map<String, Object> jobContext) throws Exception {
+		ReviewActivator.getDefault().log("Starting translation cleanup job", IStatus.INFO);
+		CDOView view = JobContextUtil.openView(jobContext);
+		try {
+			Resource resource = view.getResource(ServerConstants.WORKSPACE_RESOURCE);
+			Workspace workspace = (Workspace) resource.getContents().get(0);
+			cleanup(workspace);
+			ReviewActivator.getDefault().log("Translation cleanup job finished successfully", IStatus.INFO);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		finally {
+			if(view!=null)
+				view.close();
+		}
+
+	}
+
+	private void cleanup(Workspace workspace) {
+		EList<Project> projects = workspace.getProjects();
+		for (Project project : projects) {
+			cleanup(project);
+		}
+		
+	}
+
+	private void cleanup(Project project) {
+		ProjectVersion master = project.getMaster();
+		cleanup(master);
+		for (ProjectVersion version : project.getVersions()) {
+			cleanup(version);
+		}
+		
+	}
+
+	private void cleanup(ProjectVersion version) {
+		for (ProjectLocale locale : version.getLocales()) {
+			cleanup(locale);
+		}
+		
+		
+	}
+
+	private void cleanup(ProjectLocale locale) {
+		for (PropertyFileDescriptor descriptor : locale.getDescriptors()) {
+			cleanup(descriptor);
+		}
+		
+	}
+
+	private void cleanup(PropertyFileDescriptor descriptor) {
+		PropertyFile masterProperties = descriptor.getMaster().loadProperties();
+		Map<String, Property> map = masterProperties.asMap();
+		PropertyFile properties = descriptor.loadProperties();
+		Iterator<Property> iterator = properties.getProperties().iterator();
+		boolean hadDeletes = false;
+		while (iterator.hasNext()) {
+			Property property = (Property) iterator.next();
+			if(!map.containsKey(property.getKey()))
+			{
+				iterator.remove();
+				String message = "Removed unused translation {0} in {1}";
+				ReviewActivator.getDefault().log(MessageFormat.format(message, property.getKey(), descriptor.fullPath()), IStatus.INFO);
+				hadDeletes = true;
+			}
+		}
+		if(hadDeletes)
+		{
+			PropertyPersistenceService propertyPersistenceService = ReviewActivator.getDefault().getPersistenceService();
+			propertyPersistenceService.saveProperties(descriptor, properties);
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see de.jutzig.jabylon.scheduler.JobExecution#retryOnError()
+	 */
+	@Override
+	public boolean retryOnError() {
+		return false;
+	}
+
+}
