a056a0af99c36b9ba9a1aad1681a3e88ed8bca8b
Johannes Utzig <jutzig.dev@googlemail.com>
2012-10-10 22:57:59 +0200

    added fileutil

--- de.jutzig.jabylon.common/src/main/java/de/jutzig/jabylon/common/progress/RunnableWithProgress.java
+++ de.jutzig.jabylon.common/src/main/java/de/jutzig/jabylon/common/progress/RunnableWithProgress.java
@@ -1,8 +1,10 @@
 package de.jutzig.jabylon.common.progress;
 
+import java.io.Serializable;
+
 import org.eclipse.core.runtime.IProgressMonitor;
 
-public interface RunnableWithProgress {
+public interface RunnableWithProgress extends Serializable{
 
 	void run(IProgressMonitor monitor);
 	
