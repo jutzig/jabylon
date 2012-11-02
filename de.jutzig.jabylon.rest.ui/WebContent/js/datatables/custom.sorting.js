564ffb8d0ab6cd7fe3b274eb7ed6244900a42605
Johannes Utzig <jutzig.dev@googlemail.com>
2012-11-03 00:18:20 +0100

    datatable for filtering/pagination added

---
+++ de.jutzig.jabylon.rest.ui/WebContent/js/datatables/custom.sorting.js
@@ -0,0 +1,14 @@
+jQuery.extend( jQuery.fn.dataTableExt.oSort, {
+    "percent-pre": function ( a ) {
+        var x = (a == "-") ? 0 : a.replace( /%/, "" );
+        return parseFloat( x );
+    },
+ 
+    "percent-asc": function ( a, b ) {
+        return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+    },
+ 
+    "percent-desc": function ( a, b ) {
+        return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+    }
+} );
