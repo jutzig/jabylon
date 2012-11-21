88e46ef1153ff3f46fad8481e2a45a6765b04684
Johannes Utzig <jutzig.dev@googlemail.com>
2012-11-21 21:26:03 +0100

    additional unit tests for new checkForBom method added

--- de.jutzig.jabylon.properties/src/test/java/de/jutzig/jabylon/properties/util/PropertiesHelperTest.java
+++ de.jutzig.jabylon.properties/src/test/java/de/jutzig/jabylon/properties/util/PropertiesHelperTest.java
@@ -2,9 +2,12 @@ package de.jutzig.jabylon.properties.util;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.when;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -16,8 +19,11 @@ import java.io.StringReader;
 import java.io.StringWriter;
 import java.io.ObjectInputStream.GetField;
 
+import org.apache.tools.ant.filters.StringInputStream;
+import org.eclipse.emf.ecore.resource.ContentHandler.ByteOrderMark;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import de.jutzig.jabylon.properties.PropertiesFactory;
 import de.jutzig.jabylon.properties.Property;
@@ -39,8 +45,7 @@ public class PropertiesHelperTest {
 		try {
 			reader = loadFile("wiki_example.properties");
 			Property property = fixture.readProperty(reader);
-			assertEquals(
-					"You are reading the \".properties\" entry.\nThe exclamation mark can also mark text as comments.",
+			assertEquals("You are reading the \".properties\" entry.\nThe exclamation mark can also mark text as comments.",
 					property.getComment());
 			assertEquals("website", property.getKey());
 			assertEquals("http://en.wikipedia.org/", property.getValue());
@@ -51,8 +56,7 @@ public class PropertiesHelperTest {
 			assertEquals("English", property.getValue());
 
 			property = fixture.readProperty(reader);
-			assertEquals(
-					"The backslash below tells the application to continue reading\nthe value onto the next line.",
+			assertEquals("The backslash below tells the application to continue reading\nthe value onto the next line.",
 					property.getComment());
 			assertEquals("message", property.getKey());
 			assertEquals("Welcome to \nWikipedia!", property.getValue());
@@ -60,9 +64,7 @@ public class PropertiesHelperTest {
 			property = fixture.readProperty(reader);
 			assertEquals("Add spaces to the key", property.getComment());
 			assertEquals("key with spaces", property.getKey());
-			assertEquals(
-					"This is the value that could be looked up with the key \"key with spaces\".",
-					property.getValue());
+			assertEquals("This is the value that could be looked up with the key \"key with spaces\".", property.getValue());
 
 			property = fixture.readProperty(reader);
 			assertEquals("Unicode", property.getComment());
@@ -79,22 +81,22 @@ public class PropertiesHelperTest {
 	 * @see https://github.com/jutzig/jabylon/issues/55
 	 * @throws IOException
 	 */
-    @Test
-    public void testLoadPropertyWithUnicodeAndBOM() throws IOException{
-        BufferedReader reader = null;
-        try {
-            InputStream stream = new BufferedInputStream(loadFileAsStream("Buttons_en.seenls"));
-            fixture.checkForBom(stream);
-            reader = new BufferedReader(new InputStreamReader(stream));
-            Property property = fixture.readProperty(reader);
-            assertEquals("DTM.CASCADE_SESSION_FRAMES_TEXT", property.getKey());
-            assertEquals("Cascade", property.getValue());
-            assertEquals("test", property.getComment());
-        } finally {
-            if (reader != null)
-                reader.close();
-        }
-    }
+	@Test
+	public void testLoadPropertyWithUnicodeAndBOM() throws IOException {
+		BufferedReader reader = null;
+		try {
+			InputStream stream = new BufferedInputStream(loadFileAsStream("Buttons_en.seenls"));
+			fixture.checkForBom(stream);
+			reader = new BufferedReader(new InputStreamReader(stream));
+			Property property = fixture.readProperty(reader);
+			assertEquals("DTM.CASCADE_SESSION_FRAMES_TEXT", property.getKey());
+			assertEquals("Cascade", property.getValue());
+			assertEquals("test", property.getComment());
+		} finally {
+			if (reader != null)
+				reader.close();
+		}
+	}
 
 	@Test
 	public void testReadPropertySingleCharComment() throws IOException {
@@ -103,14 +105,14 @@ public class PropertiesHelperTest {
 			reader = loadFile("plugin.properties");
 
 			Property property = fixture.readProperty(reader);
-			assertEquals("<copyright>\n</copyright>\n\n$Id$",property.getComment());
-			assertEquals("pluginName",property.getKey());
-			assertEquals("Properties Model",property.getValue());
+			assertEquals("<copyright>\n</copyright>\n\n$Id$", property.getComment());
+			assertEquals("pluginName", property.getKey());
+			assertEquals("Properties Model", property.getValue());
 
 			property = fixture.readProperty(reader);
 			assertNull(property.getComment());
-			assertEquals("providerName",property.getKey());
-			assertEquals("www.example.org",property.getValue());
+			assertEquals("providerName", property.getKey());
+			assertEquals("www.example.org", property.getValue());
 
 			assertNull(fixture.readProperty(reader));
 
@@ -120,7 +122,6 @@ public class PropertiesHelperTest {
 		}
 	}
 
-
 	@Test
 	public void testReadUnicodePropertyWithUnicodeEscapes() throws IOException {
 
@@ -129,7 +130,7 @@ public class PropertiesHelperTest {
 
 		try {
 			Property property = fixture.readProperty(reader);
-			assertEquals("even with escaping turned of, it must still parse the escaped strings","äöÜ",property.getKey());
+			assertEquals("even with escaping turned of, it must still parse the escaped strings", "äöÜ", property.getKey());
 			assertEquals("aaa", property.getValue());
 		} finally {
 			if (reader != null)
@@ -201,18 +202,45 @@ public class PropertiesHelperTest {
 
 	}
 
-
 	private BufferedReader loadFile(String name) throws FileNotFoundException {
-		File file = new File(
-				"src/test/resources/project/master/de/jutzig/jabylon/properties/util/"
-						+ name);
+		File file = new File("src/test/resources/project/master/de/jutzig/jabylon/properties/util/" + name);
 		return new BufferedReader(new FileReader(file));
 	}
-	
+
 	private InputStream loadFileAsStream(String name) throws FileNotFoundException {
-		File file = new File(
-				"src/test/resources/project/master/de/jutzig/jabylon/properties/util/"
-						+ name);
+		File file = new File("src/test/resources/project/master/de/jutzig/jabylon/properties/util/" + name);
 		return new FileInputStream(file);
+	}
+
+	@Test
+	public void testCheckForBom() throws Exception {
+		byte[] bytes = ByteOrderMark.UTF_16BE.bytes();
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		out.write(bytes);
+		out.write("test".getBytes());
+
+		ByteArrayInputStream stream = new ByteArrayInputStream(out.toByteArray());
+		ByteOrderMark bom = fixture.checkForBom(stream);
+		assertEquals(ByteOrderMark.UTF_16BE, bom);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+		assertEquals("The reader must contain everything after the bom", "test", reader.readLine());
+
+	}
+
+	@Test
+	public void testCheckForBomNoBom() throws Exception {
+		ByteArrayInputStream stream = new ByteArrayInputStream("test".getBytes());
+		ByteOrderMark bom = fixture.checkForBom(stream);
+		assertNull(bom);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+		assertEquals("The reader must contain everything after the bom", "test", reader.readLine());
+
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testCheckForBomWrongStream() throws Exception {
+		InputStream in = Mockito.mock(InputStream.class);
+		when(in.markSupported()).thenReturn(false);
+		fixture.checkForBom(in);
 	}
 }
