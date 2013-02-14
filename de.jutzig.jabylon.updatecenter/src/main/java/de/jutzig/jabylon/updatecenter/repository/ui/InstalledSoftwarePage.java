5486b290c64952f0b04f28cffc94604bf23b0ce9
Johannes Utzig <jutzig.dev@googlemail.com>
2013-02-14 23:02:02 +0100

    added the possibility to start/stop bundles from the osgi console

--- de.jutzig.jabylon.updatecenter/src/main/java/de/jutzig/jabylon/updatecenter/repository/ui/InstalledSoftwarePage.java
+++ de.jutzig.jabylon.updatecenter/src/main/java/de/jutzig/jabylon/updatecenter/repository/ui/InstalledSoftwarePage.java
@@ -5,12 +5,13 @@ import java.util.List;
 
 import javax.inject.Inject;
 
-import org.apache.felix.bundlerepository.Resource;
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
 import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.IModel;
@@ -18,6 +19,7 @@ import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.util.string.StringValue;
 import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleException;
 import org.osgi.framework.FrameworkUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,6 +36,10 @@ import de.jutzig.jabylon.updatecenter.repository.OBRRepositoryService;
 //TODO: use different right for this
 @AuthorizeInstantiation("ACCESS_CONFIG")
 public class InstalledSoftwarePage extends GenericPage<String> {
+
+	private static final String PARAM_BUNDLE = "bundle";
+
+	private static final String PARAM_ACTION = "action";
 
 	private static final long serialVersionUID = 1L;
 
@@ -62,7 +68,7 @@ public class InstalledSoftwarePage extends GenericPage<String> {
 		Bundle bundle = FrameworkUtil.getBundle(getClass());
 		
 		List<Bundle> resources = Arrays.asList(bundle.getBundleContext().getBundles()); 
-		
+		final long targetBundleId = getPageParameters().get(PARAM_BUNDLE).toLong(-1);
 		ListView<Bundle> resourceView = new ListView<Bundle>("row", resources) {
 			
 			private static final long serialVersionUID = 1L;
@@ -70,6 +76,30 @@ public class InstalledSoftwarePage extends GenericPage<String> {
 			@Override
 			protected void populateItem(ListItem<Bundle> item) {
 				Bundle resource = item.getModelObject();
+				
+				if(resource.getBundleId()==targetBundleId) {
+					if("start".equals(getPageParameters().get(PARAM_ACTION).toOptionalString())) {
+						logger.info("Starting bundle {}", resource.getSymbolicName());
+						try {
+							resource.start();
+						} catch (BundleException e) {
+							String message = "Failed to start bundle "+resource.getSymbolicName();
+							getSession().error(message);
+							logger.error(message,e);
+						}						
+					}
+					else if("stop".equals(getPageParameters().get(PARAM_ACTION).toOptionalString())) {
+						logger.info("Stoping bundle {}", resource.getSymbolicName());
+						try {
+							resource.stop();
+						} catch (BundleException e) {
+							String message = "Failed to stop bundle "+resource.getSymbolicName();
+							getSession().error(message);
+							logger.error(message,e);
+						} 						
+					}
+				}
+				
 				String name = resource.getSymbolicName();
 				item.add(new Label("name", name));
 				item.add(new Label("version", resource.getVersion().toString()));
@@ -78,6 +108,24 @@ public class InstalledSoftwarePage extends GenericPage<String> {
 				Label stateLabel = new Label("state",bundleState.name());
 				stateLabel.add(new AttributeAppender("class", "label label-"+bundleState.getLabelClass()));
 				item.add(stateLabel);
+				
+				
+				
+				String action = null;
+				if(bundleState==BundleState.ACTIVE)
+					action = "stop";
+				else if(bundleState==BundleState.RESOLVED)
+					action = "start";
+				PageParameters params = new PageParameters();
+				if(action!=null)
+				{
+					params.set(PARAM_ACTION, action);
+					params.set(PARAM_BUNDLE, resource.getBundleId());					
+				}
+				Link<Object> actionLink = new BookmarkablePageLink<Object>(PARAM_ACTION, getPageClass(),params);
+				actionLink.setBody(Model.of(action == null ? "start" : action));
+				actionLink.setEnabled(action!=null);
+				item.add(actionLink);
 			}
 		};
 		add(resourceView);
