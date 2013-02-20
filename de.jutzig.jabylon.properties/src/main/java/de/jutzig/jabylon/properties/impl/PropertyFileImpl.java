d07d6a30baed8b0f57df63625535d0ddb1330504
Johannes Utzig <jutzig.dev@googlemail.com>
2013-02-20 22:36:08 +0100

    synchronized internal iterations in PropertyFile

--- de.jutzig.jabylon.properties/src/main/java/de/jutzig/jabylon/properties/impl/PropertyFileImpl.java
+++ de.jutzig.jabylon.properties/src/main/java/de/jutzig/jabylon/properties/impl/PropertyFileImpl.java
@@ -23,24 +23,27 @@ import de.jutzig.jabylon.properties.PropertyFile;
 import de.jutzig.jabylon.properties.util.SynchronizedEList;
 
 /**
- * <!-- begin-user-doc -->
- * An implementation of the model object '<em><b>Property File</b></em>'.
- * <!-- end-user-doc -->
+ * <!-- begin-user-doc --> An implementation of the model object '
+ * <em><b>Property File</b></em>'. <!-- end-user-doc -->
  * <p>
  * The following features are implemented:
  * <ul>
- *   <li>{@link de.jutzig.jabylon.properties.impl.PropertyFileImpl#getProperties <em>Properties</em>}</li>
- *   <li>{@link de.jutzig.jabylon.properties.impl.PropertyFileImpl#getLicenseHeader <em>License Header</em>}</li>
+ * <li>{@link de.jutzig.jabylon.properties.impl.PropertyFileImpl#getProperties
+ * <em>Properties</em>}</li>
+ * <li>
+ * {@link de.jutzig.jabylon.properties.impl.PropertyFileImpl#getLicenseHeader
+ * <em>License Header</em>}</li>
  * </ul>
  * </p>
- *
+ * 
  * @generated
  */
 public class PropertyFileImpl extends CDOObjectImpl implements PropertyFile {
 	/**
-	 * The default value of the '{@link #getLicenseHeader() <em>License Header</em>}' attribute.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * The default value of the '{@link #getLicenseHeader()
+	 * <em>License Header</em>}' attribute. <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
+	 * 
 	 * @see #getLicenseHeader()
 	 * @generated
 	 * @ordered
@@ -48,8 +51,8 @@ public class PropertyFileImpl extends CDOObjectImpl implements PropertyFile {
 	protected static final String LICENSE_HEADER_EDEFAULT = null;
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	protected PropertyFileImpl() {
@@ -57,8 +60,8 @@ public class PropertyFileImpl extends CDOObjectImpl implements PropertyFile {
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
@@ -67,8 +70,8 @@ public class PropertyFileImpl extends CDOObjectImpl implements PropertyFile {
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
@@ -77,29 +80,30 @@ public class PropertyFileImpl extends CDOObjectImpl implements PropertyFile {
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * synchronized list to allow access from multiple threads
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> synchronized list to allow access from multiple
+	 * threads <!-- end-user-doc -->
+	 * 
 	 * @generated NOT
 	 */
 	@SuppressWarnings("unchecked")
 	public EList<Property> getProperties() {
-		EList<Property> properties = (EList<Property>)eDynamicGet(PropertiesPackage.PROPERTY_FILE__PROPERTIES, PropertiesPackage.Literals.PROPERTY_FILE__PROPERTIES, true, true);
+		EList<Property> properties = (EList<Property>) eDynamicGet(PropertiesPackage.PROPERTY_FILE__PROPERTIES,
+				PropertiesPackage.Literals.PROPERTY_FILE__PROPERTIES, true, true);
 		return new SynchronizedEList<Property>(properties);
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public String getLicenseHeader() {
-		return (String)eDynamicGet(PropertiesPackage.PROPERTY_FILE__LICENSE_HEADER, PropertiesPackage.Literals.PROPERTY_FILE__LICENSE_HEADER, true, true);
+		return (String) eDynamicGet(PropertiesPackage.PROPERTY_FILE__LICENSE_HEADER, PropertiesPackage.Literals.PROPERTY_FILE__LICENSE_HEADER, true, true);
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public void setLicenseHeader(String newLicenseHeader) {
@@ -107,114 +111,119 @@ public class PropertyFileImpl extends CDOObjectImpl implements PropertyFile {
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated NOT
 	 */
 	public Property getProperty(String key) {
-		for (Property property : getProperties()) {
-			if(key.equals(property.getKey()))
-				return property;
+		EList<Property> eList = getProperties();
+		synchronized (eList) {
+			for (Property property : eList) {
+				if (key.equals(property.getKey()))
+					return property;
+			}
 		}
 		return null;
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated NOT
 	 */
 	public Map<String, Property> asMap() {
 		EList<Property> properties = getProperties();
-		Map<String, Property> map = new HashMap<String, Property>(properties.size());
-		for (Property property : getProperties()) {
-			map.put(property.getKey(), property);
+		synchronized (properties) {
+			Map<String, Property> map = new HashMap<String, Property>(properties.size());
+			for (Property property : getProperties()) {
+				map.put(property.getKey(), property);
+			}			
+			return map;
 		}
-		return map;
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	public NotificationChain eInverseRemove(InternalEObject otherEnd, int featureID, NotificationChain msgs) {
 		switch (featureID) {
-			case PropertiesPackage.PROPERTY_FILE__PROPERTIES:
-				return ((InternalEList<?>)getProperties()).basicRemove(otherEnd, msgs);
+		case PropertiesPackage.PROPERTY_FILE__PROPERTIES:
+			return ((InternalEList<?>) getProperties()).basicRemove(otherEnd, msgs);
 		}
 		return super.eInverseRemove(otherEnd, featureID, msgs);
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	public Object eGet(int featureID, boolean resolve, boolean coreType) {
 		switch (featureID) {
-			case PropertiesPackage.PROPERTY_FILE__PROPERTIES:
-				return getProperties();
-			case PropertiesPackage.PROPERTY_FILE__LICENSE_HEADER:
-				return getLicenseHeader();
+		case PropertiesPackage.PROPERTY_FILE__PROPERTIES:
+			return getProperties();
+		case PropertiesPackage.PROPERTY_FILE__LICENSE_HEADER:
+			return getLicenseHeader();
 		}
 		return super.eGet(featureID, resolve, coreType);
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@SuppressWarnings("unchecked")
 	@Override
 	public void eSet(int featureID, Object newValue) {
 		switch (featureID) {
-			case PropertiesPackage.PROPERTY_FILE__PROPERTIES:
-				getProperties().clear();
-				getProperties().addAll((Collection<? extends Property>)newValue);
-				return;
-			case PropertiesPackage.PROPERTY_FILE__LICENSE_HEADER:
-				setLicenseHeader((String)newValue);
-				return;
+		case PropertiesPackage.PROPERTY_FILE__PROPERTIES:
+			getProperties().clear();
+			getProperties().addAll((Collection<? extends Property>) newValue);
+			return;
+		case PropertiesPackage.PROPERTY_FILE__LICENSE_HEADER:
+			setLicenseHeader((String) newValue);
+			return;
 		}
 		super.eSet(featureID, newValue);
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	public void eUnset(int featureID) {
 		switch (featureID) {
-			case PropertiesPackage.PROPERTY_FILE__PROPERTIES:
-				getProperties().clear();
-				return;
-			case PropertiesPackage.PROPERTY_FILE__LICENSE_HEADER:
-				setLicenseHeader(LICENSE_HEADER_EDEFAULT);
-				return;
+		case PropertiesPackage.PROPERTY_FILE__PROPERTIES:
+			getProperties().clear();
+			return;
+		case PropertiesPackage.PROPERTY_FILE__LICENSE_HEADER:
+			setLicenseHeader(LICENSE_HEADER_EDEFAULT);
+			return;
 		}
 		super.eUnset(featureID);
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	public boolean eIsSet(int featureID) {
 		switch (featureID) {
-			case PropertiesPackage.PROPERTY_FILE__PROPERTIES:
-				return !getProperties().isEmpty();
-			case PropertiesPackage.PROPERTY_FILE__LICENSE_HEADER:
-				return LICENSE_HEADER_EDEFAULT == null ? getLicenseHeader() != null : !LICENSE_HEADER_EDEFAULT.equals(getLicenseHeader());
+		case PropertiesPackage.PROPERTY_FILE__PROPERTIES:
+			return !getProperties().isEmpty();
+		case PropertiesPackage.PROPERTY_FILE__LICENSE_HEADER:
+			return LICENSE_HEADER_EDEFAULT == null ? getLicenseHeader() != null : !LICENSE_HEADER_EDEFAULT.equals(getLicenseHeader());
 		}
 		return super.eIsSet(featureID);
 	}
 
-} //PropertyFileImpl
+} // PropertyFileImpl
