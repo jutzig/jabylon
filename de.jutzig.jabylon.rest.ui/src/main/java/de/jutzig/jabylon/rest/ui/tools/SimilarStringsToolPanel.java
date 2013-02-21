4ce1849f0f620d1860def0869df7ceb87bddbf4b
Johannes Utzig <jutzig.dev@googlemail.com>
2013-02-21 18:39:57 +0100

    close #77 Similar Strings tool loads files all values are now retrieved directly from the index

--- de.jutzig.jabylon.rest.ui/src/main/java/de/jutzig/jabylon/rest/ui/tools/SimilarStringsToolPanel.java
+++ de.jutzig.jabylon.rest.ui/src/main/java/de/jutzig/jabylon/rest/ui/tools/SimilarStringsToolPanel.java
@@ -1,7 +1,8 @@
 /**
- * 
+ *
  */
 package de.jutzig.jabylon.rest.ui.tools;
+
 
 import java.io.IOException;
 import java.io.Serializable;
@@ -11,15 +12,18 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Set;
 import java.util.TreeSet;
-import java.util.concurrent.ExecutionException;
 
 import javax.inject.Inject;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.CorruptIndexException;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.BooleanClause.Occur;
+import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.FuzzyLikeThisQuery;
 import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.util.Version;
 import org.apache.wicket.AttributeModifier;
@@ -32,130 +36,119 @@ import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.GenericPanel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import de.jutzig.jabylon.index.properties.QueryService;
 import de.jutzig.jabylon.index.properties.SearchResult;
+import de.jutzig.jabylon.properties.PropertiesFactory;
 import de.jutzig.jabylon.properties.Property;
-import de.jutzig.jabylon.properties.PropertyFile;
 import de.jutzig.jabylon.properties.PropertyFileDescriptor;
 import de.jutzig.jabylon.resources.persistence.PropertyPersistenceService;
 import de.jutzig.jabylon.rest.ui.model.PropertyPair;
 import de.jutzig.jabylon.rest.ui.wicket.pages.ResourcePage;
 
+
 /**
  * @author Johannes Utzig (jutzig.dev@googlemail.com)
- *
  */
-public class SimilarStringsToolPanel extends GenericPanel<PropertyPair> {
+public class SimilarStringsToolPanel
+    extends GenericPanel<PropertyPair>
+{
 
-	private static final long serialVersionUID = 1L;
-	private static Logger logger = LoggerFactory.getLogger(SimilarStringsToolPanel.class);
-	@Inject
-	private transient QueryService queryService;
-	@Inject
-	private transient PropertyPersistenceService persistenceService;
-	
-	public SimilarStringsToolPanel(String id, IModel<PropertyPair> model) {
-		super(id, model);;
+    private static final long serialVersionUID = 1L;
+    private static Logger logger = LoggerFactory.getLogger(SimilarStringsToolPanel.class);
+    @Inject
+    private transient QueryService queryService;
+    @Inject
+    private transient PropertyPersistenceService persistenceService;
 
-	}
-	
-	@Override
-	protected void onBeforeRender() {
-		List<Similarity> result = doSearch(getModel());
-		ListView<Similarity> list = new ListView<Similarity>("children",result) {
-			
-			private static final long serialVersionUID = 1L;
 
-			@Override
-			protected void populateItem(ListItem<Similarity> item) {
-				Similarity similarity = item.getModelObject();
-				item.add(new Label("template",similarity.getOriginal()));
-				item.add(new Label("translation",similarity.getTranslation()));
-				item.add(new AttributeAppender("title", similarity.getFullPath()));
-				PageParameters params = new PageParameters();
-				
-				URI uri = URI.createURI(similarity.getUri());
-				for(int i=0;i<uri.segmentCount();i++) {
-					params.set(i, uri.segment(i));
-				}
-				params.add("key", similarity.getKey());
-				item.add(new BookmarkablePageLink<Void>("link", ResourcePage.class, params));
-				WebMarkupContainer progress = new WebMarkupContainer("similarity");
-				item.add(progress);
-				progress.add(new AttributeModifier("style", "width: " + similarity.getSimilarity() + "%"));
-				
-			}
-		};
-		addOrReplace(list);
-		super.onBeforeRender();
-	}
+    public SimilarStringsToolPanel(String id, IModel<PropertyPair> model)
+    {
+        super(id, model);
+        ;
 
-	
-	protected List<Similarity> doSearch(IModel<PropertyPair> model)
-	{
+    }
 
-		
-		PropertyPair pair = model.getObject();
-		if(pair==null || pair.getOriginal()==null)
-			return Collections.emptyList();
+
+    @Override
+    protected void onBeforeRender()
+    {
+        List<Similarity> result = doSearch(getModel());
+        ListView<Similarity> list = new ListView<Similarity>("children", result)
+        {
+
+            private static final long serialVersionUID = 1L;
+
+
+            @Override
+            protected void populateItem(ListItem<Similarity> item)
+            {
+                Similarity similarity = item.getModelObject();
+                item.add(new Label("template", similarity.getOriginal()));
+                item.add(new Label("translation", similarity.getTranslation()));
+                item.add(new AttributeAppender("title", similarity.getFullPath()));
+                PageParameters params = new PageParameters();
+
+                URI uri = URI.createURI(similarity.getUri());
+                for (int i = 0; i < uri.segmentCount(); i++)
+                {
+                    params.set(i, uri.segment(i));
+                }
+                params.add("key", similarity.getKey());
+                item.add(new BookmarkablePageLink<Void>("link", ResourcePage.class, params));
+                WebMarkupContainer progress = new WebMarkupContainer("similarity");
+                item.add(progress);
+                progress.add(new AttributeModifier("style", "width: " + similarity.getSimilarity() + "%"));
+
+            }
+        };
+        addOrReplace(list);
+        super.onBeforeRender();
+    }
+
+
+    protected List<Similarity> doSearch(IModel<PropertyPair> model)
+    {
+
+        PropertyPair pair = model.getObject();
+        if (pair == null || pair.getOriginal() == null)
+            return Collections.emptyList();
         FuzzyLikeThisQuery query = new FuzzyLikeThisQuery(10, new StandardAnalyzer(Version.LUCENE_35));
         query.addTerms(pair.getOriginal(), QueryService.FIELD_VALUE, 0.6f, 3);
-        SearchResult result = queryService.search(query,20);
+        //make sure we only look for templates, not translations
+        query.addTerms(QueryService.MASTER, QueryService.FIELD_LOCALE, 0.99f, 3);
+        SearchResult result = queryService.search(query, 20);
 
-        if(result==null)
+        if (result == null)
             return Collections.emptyList();
         Set<Similarity> resultList = new TreeSet<SimilarStringsToolPanel.Similarity>();
         TopDocs topDocs = result.getTopDocs();
         ScoreDoc[] doc = topDocs.scoreDocs;
+        int hitNumber = 0;
         for (ScoreDoc scoreDoc : doc)
         {
-            if(scoreDoc.score<0.20)
+            if (scoreDoc.score < 0.20)
                 continue;
-            
+
             try
             {
                 Document document = result.getSearcher().doc(scoreDoc.doc);
-                
-                PropertyFileDescriptor descriptor = queryService.getDescriptor(document);
-
-            if(descriptor==null)
-                continue;
-            PropertyFileDescriptor slave = getSlave(descriptor,pair.getLanguage());
-            if(slave==null)
-                continue;
-            //TODO: it would be better to get this from the index
-            PropertyFile properties = persistenceService.loadProperties(slave);
-            String key = document.get(QueryService.FIELD_KEY);
-            
-            if(slave.cdoID().equals(pair.getDescriptorID()) && pair.getKey().equals(key))
-            	continue; //that means we found the current property as a similarity. Very helpful...
-            Property property = properties.getProperty(key);
-//            if(pair.getTranslated()!=null && pair.getTranslated().equals(property.getValue()))
-//            	//the translations are identical. That won't help
-//            	continue;
-            
-            if(property==null || property.getValue()==null)
-                continue;
-            
-            String uri = slave.getProjectLocale().getParent().getParent().getName() + "/" + slave.getProjectLocale().getParent().getName() + "/" + slave.getProjectLocale().getName()+ slave.getLocation().toString();
-            Similarity similarity = new Similarity(document.get(QueryService.FIELD_VALUE), property.getValue(),(int)(scoreDoc.score*100), document.get(QueryService.FIELD_FULL_PATH), uri, property.getKey());
-            resultList.add(similarity);
+                Similarity similarity = createSimilarity(document, pair.getLanguage(), (int)(scoreDoc.score * 100),hitNumber++);
+                if (similarity == null)
+                    continue;
+                resultList.add(similarity);
             }
             catch (CorruptIndexException e)
             {
-            	logger.error("Failed to find similar strings", e);
+                logger.error("Failed to find similar strings", e);
             }
             catch (IOException e)
             {
-            	logger.error("Failed to find similar strings", e);
-			} catch (ExecutionException e) {
-				logger.error("Failed to find load properties", e);
-			}
+                logger.error("Failed to find similar strings", e);
+            }
 
         }
         try
@@ -164,36 +157,88 @@ public class SimilarStringsToolPanel extends GenericPanel<PropertyPair> {
         }
         catch (IOException e)
         {
-        	logger.error("Failed to close searcher", e);
+            logger.error("Failed to close searcher", e);
         }
         return new ArrayList<SimilarStringsToolPanel.Similarity>(resultList);
 
-	}
-	
-    private PropertyFileDescriptor getSlave(PropertyFileDescriptor descriptor, Locale language)
-    {
-    	EList<PropertyFileDescriptor> translations = descriptor.getDerivedDescriptors();
-    	for (PropertyFileDescriptor translation : translations) {
-			if(language.equals(translation.getVariant()))
-				return translation;
-		}
-    	return null;
     }
 
-    
-    public static class Similarity implements Serializable, Comparable<Similarity>
+
+    private Similarity createSimilarity(Document masterDoc, Locale language, int score, int hitNumber)
+        throws CorruptIndexException, IOException
     {
-        
-		private static final long serialVersionUID = 1L;
-		private String original;
+        PropertyFileDescriptor descriptor = queryService.getDescriptor(masterDoc);
+        if (descriptor == null)
+            return null;
+        String key = masterDoc.get(QueryService.FIELD_KEY);
+        BooleanQuery query = new BooleanQuery();
+        query.add(new TermQuery(new Term(QueryService.FIELD_TEMPLATE_LOCATION, descriptor.getLocation().toString())),Occur.MUST);
+        query.add(new TermQuery(new Term(QueryService.FIELD_LOCALE, language.toString())), Occur.MUST);
+        query.add(new TermQuery(new Term(QueryService.FIELD_KEY, key)), Occur.MUST);
+
+        SearchResult searchResult = queryService.search(query, 1);
+        try{
+            TopDocs topDocs = searchResult.getTopDocs();
+            if (topDocs.totalHits == 0)
+                return null;
+            Document translationDoc = searchResult.getSearcher().doc(searchResult.getTopDocs().scoreDocs[0].doc);
+            Property property = PropertiesFactory.eINSTANCE.createProperty();
+            property.setKey(key);
+            property.setValue(translationDoc.get(QueryService.FIELD_VALUE));
+            property.setComment(translationDoc.get(QueryService.FIELD_COMMENT));
+            PropertyFileDescriptor slave = queryService.getDescriptor(translationDoc);
+            if(slave==null)
+                return null;
+            PropertyPair pair = getModelObject();
+            //that would mean we found the current property, which is (of course) similar :-)
+            if(pair.getKey().equals(key) && slave.cdoID().equals(pair.getDescriptorID()))
+                return null;
+
+            Similarity similarity = new Similarity(masterDoc.get(QueryService.FIELD_VALUE),
+                                                   translationDoc.get(QueryService.FIELD_VALUE),
+                                                   score,
+                                                   masterDoc.get(QueryService.FIELD_FULL_PATH),
+                                                   slave.toURI().toString(),
+                                                   key,
+                                                   hitNumber);
+            return similarity;
+        }
+        finally {
+
+            try
+            {
+                searchResult.getSearcher().close();
+            }
+            catch (IOException e)
+            {
+                logger.error("Failed to close searcher", e);
+            }
+
+        }
+    }
+
+    public static class Similarity
+        implements Serializable, Comparable<Similarity>
+    {
+
+        private static final long serialVersionUID = 1L;
+        private String original;
         private String translation;
         private int similarity;
-		private String fullPath;
-		private String uri;
-		private String key;
+        private String fullPath;
+        private String uri;
+        private String key;
+        /** the order number is to make sure compareTo never returns 0 */
+        private int orderNumber;
 
 
-        public Similarity(String original, String translation, int similartiy, String fullPath, String uri, String key)
+        public Similarity(String original,
+                          String translation,
+                          int similartiy,
+                          String fullPath,
+                          String uri,
+                          String key,
+                          int orderNumber)
         {
             super();
             this.original = original;
@@ -202,81 +247,116 @@ public class SimilarStringsToolPanel extends GenericPanel<PropertyPair> {
             this.fullPath = fullPath;
             this.uri = uri;
             this.key = key;
+            this.orderNumber = orderNumber;
         }
-        
 
-        public String getKey() {
-			return key;
-		}
-        
+
+        public String getKey()
+        {
+            return key;
+        }
+
+
         public String getOriginal()
         {
             return original;
         }
+
 
         public String getTranslation()
         {
             return translation;
         }
 
+
         public int getSimilarity()
         {
             return similarity;
         }
-        
-        public String getFullPath() {
-			return fullPath;
-		}
-        
-        public String getUri() {
-			return uri;
-		}
 
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + ((fullPath == null) ? 0 : fullPath.hashCode());
-			result = prime * result + ((original == null) ? 0 : original.hashCode());
-			result = prime * result + similarity;
-			result = prime * result + ((translation == null) ? 0 : translation.hashCode());
-			return result;
-		}
 
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			Similarity other = (Similarity) obj;
-			if (fullPath == null) {
-				if (other.fullPath != null)
-					return false;
-			} else if (!fullPath.equals(other.fullPath))
-				return false;
-			if (original == null) {
-				if (other.original != null)
-					return false;
-			} else if (!original.equals(other.original))
-				return false;
-			if (similarity != other.similarity)
-				return false;
-			if (translation == null) {
-				if (other.translation != null)
-					return false;
-			} else if (!translation.equals(other.translation))
-				return false;
-			return true;
-		}
+        public String getFullPath()
+        {
+            return fullPath;
+        }
 
-		@Override
-		public int compareTo(Similarity o) {
-			return o.getSimilarity() - getSimilarity();
-		}
-        
-        
+
+        public String getUri()
+        {
+            return uri;
+        }
+
+
+        @Override
+        public int hashCode()
+        {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((fullPath == null) ? 0 : fullPath.hashCode());
+            result = prime * result + ((uri == null) ? 0 : uri.hashCode());
+            result = prime * result + ((original == null) ? 0 : original.hashCode());
+            result = prime * result + similarity;
+            result = prime * result + ((translation == null) ? 0 : translation.hashCode());
+            return result;
+        }
+
+
+        @Override
+        public boolean equals(Object obj)
+        {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            Similarity other = (Similarity)obj;
+            if (fullPath == null)
+            {
+                if (other.fullPath != null)
+                    return false;
+            }
+            else if (!fullPath.equals(other.fullPath))
+                return false;
+            if (uri == null)
+            {
+                if (other.uri != null)
+                    return false;
+            }
+            else if (!uri.equals(other.uri))
+                return false;
+            if (original == null)
+            {
+                if (other.original != null)
+                    return false;
+            }
+            else if (!original.equals(other.original))
+                return false;
+            if (similarity != other.similarity)
+                return false;
+            if (translation == null)
+            {
+                if (other.translation != null)
+                    return false;
+            }
+            else if (!translation.equals(other.translation))
+                return false;
+            return true;
+        }
+
+
+        @Override
+        public int compareTo(Similarity o)
+        {
+           int result = o.getSimilarity() - getSimilarity();
+           if(result==0)
+           {
+               if(equals(o))
+                   return 0;
+               return orderNumber-o.orderNumber;
+           }
+           return result;
+        }
+
     }
 }
