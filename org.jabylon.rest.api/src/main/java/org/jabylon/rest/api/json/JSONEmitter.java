60434188952443f32dfc423a5bd3b0ba7146fdee
Johannes Utzig <jutzig.dev@googlemail.com>
2013-12-11 23:32:28 +0100

    closes #134 - Add license header

--- org.jabylon.rest.api/src/main/java/org/jabylon/rest/api/json/JSONEmitter.java
+++ org.jabylon.rest.api/src/main/java/org/jabylon/rest/api/json/JSONEmitter.java
@@ -1,9 +1,10 @@
-/*
- * JSONEmitter.java
+/**
+ * (C) Copyright 2013 Jabylon (http://www.jabylon.org) and others.
  *
- * created at 13.09.2012 by utzig <YOURMAILADDRESS>
- *
- * Copyright (c) SEEBURGER AG, Germany. All Rights Reserved.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  */
 package org.jabylon.rest.api.json;
 
