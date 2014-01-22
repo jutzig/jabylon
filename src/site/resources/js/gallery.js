2ae4bfa3d30ea238d336e0434ea8874285713139
Johannes Utzig <jutzig.dev@googlemail.com>
2014-01-23 00:41:51 +0100

    added feature screenshots

--- src/site/resources/js/gallery.js
+++ src/site/resources/js/gallery.js
@@ -5,16 +5,24 @@
 $(document).ready(function() {
 	
 	$("ul.thumbnails a:has(img)").attr("rel", function() {
-
 		return "gallery";
 	});
+	
+	$("ul.thumbnails a:has(img)").attr("href", function() {
+		return $(this).find("img").attr("src");
+	});
+	
+	$("ul.thumbnails a:has(img)").attr("title", function() {
+		return $(this).siblings("p.gallery-text").text();
+	});	
 
 	$("ul.thumbnails a:has(img)").fancybox({
 		'transitionIn' : 'elastic',
 		'transitionOut' : 'elastic',
 		'speedIn' : 600,
 		'speedOut' : 200,
-		'overlayShow' : false
+		'overlayShow' : false,
+		'titlePosition' : 'over'
 
 	});
 });
