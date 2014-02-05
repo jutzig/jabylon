d15b6e4789a395d58de2016b92393217ae7d30d8
Sascha Vogt <sascha@vogt-neuenbuerg.de>
2014-02-05 09:38:22 +0100

    Fix license header in GitVersionConfigPanel.java
    refs #188

--- org.jabylon.team.git/src/main/java/org/jabylon/team/git/config/GitVersionConfigPanel.java
+++ org.jabylon.team.git/src/main/java/org/jabylon/team/git/config/GitVersionConfigPanel.java
@@ -1,11 +1,11 @@
 /**
-* (C) Copyright 2013 Jabylon (http://www.jabylon.org) and others.
-*
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*/
+ * (C) Copyright 2013 Jabylon (http://www.jabylon.org) and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.jabylon.team.git.config;
 
 import org.apache.wicket.markup.html.WebMarkupContainer;
@@ -28,11 +28,11 @@ import org.osgi.service.prefs.Preferences;
 
 public class GitVersionConfigPanel extends BasicPanel<ProjectVersion> {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public GitVersionConfigPanel(String id, IModel<ProjectVersion> model, Preferences config) {
-		super(id, model);
-		
+    public GitVersionConfigPanel(String id, IModel<ProjectVersion> model, Preferences config) {
+        super(id, model);
+
         PreferencesPropertyModel pushRefspecModel = new PreferencesPropertyModel(config, GitConstants.KEY_PUSH_REFSPEC, "");
         ControlGroup pushRefspecGroup = new ControlGroup("push-refspec-group", nls("push.refspec.label"),nls("push.refspec.help"));
         TextField<String> refspecTextField = new TextField<String>("push-refspec",pushRefspecModel);
@@ -41,70 +41,70 @@ public class GitVersionConfigPanel extends BasicPanel<ProjectVersion> {
         refspecTextField.setConvertEmptyInputStringToNull(true);
         pushRefspecGroup.add(refspecTextField);
         add(pushRefspecGroup);
-	}
+    }
 
-	public static class GitVersionConfigSection extends AbstractConfigSection<ProjectVersion>{
+    public static class GitVersionConfigSection extends AbstractConfigSection<ProjectVersion>{
 
-	    private static final long serialVersionUID = 1L;
+        private static final long serialVersionUID = 1L;
 
-	    private boolean gitSelected(IModel<ProjectVersion> model) {
-	        return "Git".equals(getProject(model).getTeamProvider());
-	    }
+        private boolean gitSelected(IModel<ProjectVersion> model) {
+            return "Git".equals(getProject(model).getTeamProvider());
+        }
 
-	    Project getProject(IModel<ProjectVersion> model) {
-	    	if (model instanceof AttachableModel) {
-				AttachableModel<ProjectVersion> m = (AttachableModel<ProjectVersion>) getModel();
-				return (Project) m.getParent().getObject();
-			}
-	    	return model.getObject().getParent();
-	    }
-	    
-
-	    @Override
-	    public WebMarkupContainer doCreateContents(String id, IModel<ProjectVersion> input, Preferences config) {
-	    	GitVersionConfigPanel panel = new GitVersionConfigPanel(id, input, config);
-	        panel.setVisible(gitSelected(input));
-	        return panel;
-	    }
-
-	    @Override
-	    public boolean isVisible(IModel<ProjectVersion> input, Preferences config) {
-	        return gitSelected(input) && super.isVisible(input, config);
-	    }
+        Project getProject(IModel<ProjectVersion> model) {
+            if (model instanceof AttachableModel) {
+                AttachableModel<ProjectVersion> m = (AttachableModel<ProjectVersion>) getModel();
+                return (Project) m.getParent().getObject();
+            }
+            return model.getObject().getParent();
+        }
 
 
-	    @Override
-	    public void commit(IModel<ProjectVersion> input, Preferences config) {
-	        // nothing to do
+        @Override
+        public WebMarkupContainer doCreateContents(String id, IModel<ProjectVersion> input, Preferences config) {
+            GitVersionConfigPanel panel = new GitVersionConfigPanel(id, input, config);
+            panel.setVisible(gitSelected(input));
+            return panel;
+        }
 
-	    }
+        @Override
+        public boolean isVisible(IModel<ProjectVersion> input, Preferences config) {
+            return gitSelected(input) && super.isVisible(input, config);
+        }
 
-	    @Override
-	    public String getRequiredPermission() {
-	    	Project project = getProject(getModel());
-	        String projectName = null;
-	        if(project!=null)
-	            projectName = project.getName();
-	        return CommonPermissions.constructPermission(CommonPermissions.PROJECT,projectName,CommonPermissions.ACTION_CONFIG);
-	    }
 
-	}
-	
-	private static class RefspecValidator implements IValidator<String> {
+        @Override
+        public void commit(IModel<ProjectVersion> input, Preferences config) {
+            // nothing to do
 
-		private static final long serialVersionUID = 1L;
+        }
 
-		@Override
-		public void validate(IValidatable<String> validatable) {
-			String value = validatable.getValue();
-			try {
-				new RefSpec(value);
-			} catch (Exception e) {
-				validatable.error(new ValidationError(e.getMessage()));
-			}
-			
-		}
-		
-	}
-	
+        @Override
+        public String getRequiredPermission() {
+            Project project = getProject(getModel());
+            String projectName = null;
+            if(project!=null)
+                projectName = project.getName();
+            return CommonPermissions.constructPermission(CommonPermissions.PROJECT,projectName,CommonPermissions.ACTION_CONFIG);
+        }
+
+    }
+
+    private static class RefspecValidator implements IValidator<String> {
+
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public void validate(IValidatable<String> validatable) {
+            String value = validatable.getValue();
+            try {
+                new RefSpec(value);
+            } catch (Exception e) {
+                validatable.error(new ValidationError(e.getMessage()));
+            }
+
+        }
+
+    }
+
 }
